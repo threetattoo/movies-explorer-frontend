b7cfb3423f26d359ae65da68f3648cd435254cd2
Threetattoo <mikhailpazyuk@yandex.ru>
2021-12-03 04:05:51 +0300

    add main and promo section

---
+++ src/components/Promo/Promo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Promo.css';
+
+function Promo() {
+    return (
+        <section className="promo">
+            <div className="promo__logo"></div>
+            <div className="promo__title">
+                Учебный проект студента факультета Веб-разработки.
+            </div>
+        </section>
+    );
+}
+
+export default Promo;
