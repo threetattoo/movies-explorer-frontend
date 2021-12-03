33bef55158d375326fd4c291fe4a45c128015ccb
Threetattoo <mikhailpazyuk@yandex.ru>
2021-12-03 21:19:37 +0300

    add techs component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Main.css';
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
+import Techs from '../Techs/Techs';
 
 function Main() {
     return (
         <main className="main">
             <Promo />
             <AboutProject />
+            <Techs />
         </main>
     );
 }
