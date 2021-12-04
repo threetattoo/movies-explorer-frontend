39edd9f42fa1f75b683dd79df4c1d0a8e2a2ebfb
Threetattoo <mikhailpazyuk@yandex.ru>
2021-12-04 03:55:42 +0300

    add footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return(
+        <footer className="footer">
+            <p className="footer__info">
+                Учебный проект Яндекс.Практикум х BeatFilm.
+            </p>
+            <hr className="footer__line" />
+            <div className="footer__container">
+                <p className="footer__copyright">© 2021</p>
+                <ul className="footer__list">
+                    <li className="footer__item">
+                        <a className="footer__link" href="https://practicum.yandex.ru" target="_blank" rel="noreferrer">Яндекс.Практикум</a>
+                    </li>
+                    <li className="footer__item">
+                        <a className="footer__link" href="https://github.com/threetattoo/" target="_blank" rel="noreferrer">Github</a>
+                    </li>
+                    <li className="footer__item">
+                        <a className="footer__link" href="https://facebook.com" target="_blank" rel="noreferrer">Facebook</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
