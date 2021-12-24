f73c478670a1958b22482451b84a575acfcba884
Threetattoo <mikhailpazyuk@yandex.ru>
2021-12-24 20:19:22 +0300

    add button more functionality

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Route, Switch } from 'react-router-dom';
 import MoviesCard from '../../components/MoviesCard/MoviesCard';
-import More from '../../components/More/More';
 import './MoviesCardList.css';
 
 function MoviesCardList({foundedMovies}) {
+    const [numberOfCardsToShow, setNumberOfCardsToShow] = useState(0);
+
+    const numberOfInitialCards = () => {
+        if (window.innerWidth > 570) {
+            setNumberOfCardsToShow(7);
+        } else if (window.innerWidth <= 570) {
+            setNumberOfCardsToShow(5);
+        } else {
+            return;
+        }
+    };
+
+    useEffect(() => {
+        numberOfInitialCards();
+        window.addEventListener('resize', () => {
+            setTimeout(numberOfInitialCards, 2000);
+        });
+           
+        return () => {
+            window.removeEventListener('resize', numberOfInitialCards);
+        };
+    }, []);
+
+    function handleClickMore() {
+        if (window.innerWidth >= 570) {
+            setNumberOfCardsToShow(numberOfCardsToShow + 7);
+        } else {
+            setNumberOfCardsToShow(numberOfCardsToShow + 5);
+        }
+    }
+    console.log(numberOfCardsToShow);
     return(
         <Switch>
             <Route path="/movies">
                 <section className="movies">
+                {foundedMovies && (
+                    <>
                     <ul className="movies-card__list">
                         {foundedMovies.map((movie) => {
                             return (
@@ -17,9 +49,16 @@ function MoviesCardList({foundedMovies}) {
                                     movie={movie}
                                 />
                             )
-                        })}
+                        }).slice(0, numberOfCardsToShow)}
                     </ul>
-                    <More />
+                    {(foundedMovies.length > numberOfCardsToShow) &&
+                    (<div className="more">
+                        <button className="more__button" onClick={handleClickMore}>
+                            Ещё
+                        </button>
+                    </div>)}
+                    </>
+                )}
                 </section>
             </Route>
             <Route path="/saved-movies">
