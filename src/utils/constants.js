7be72b1ad838f1aa635db4dec4dafc044af9ef38
Threetattoo <mikhailpazyuk@yandex.ru>
2022-01-06 07:14:06 +0300

    fix: api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const BEATFILM_API_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 export const BEATFILM_DOMAIN_URL = 'https://api.nomoreparties.co';
-//export const MAIN_API_URL = 'https://api.threetattoo-diploma.nomoredomains.rocks';
-export const MAIN_API_URL = 'http://localhost:3000';
+export const MAIN_API_URL = 'https://api.threetattoo-diploma.nomoredomains.rocks';
+//export const MAIN_API_URL = 'http://localhost:3000';
